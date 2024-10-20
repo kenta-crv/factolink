0c0fbc9bba108e39f6265e5bf26d849740d68232
kenta-crv <okuyamakenta@gmail.com>
2024-10-20 16:23:04 +0900

    first fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   #管理者アカウント
   devise_for :admins, controllers: {
     registrations: 'admins/registrations',
@@ -9,17 +8,6 @@ Rails.application.routes.draw do
   resources :admins, only: [:show]
 
   root to: 'top#index' #トップページ
-  #特集
-  get 'faq' => 'top#faq'
-  get 'co' => 'top#co'
-  get 'question' => 'top#question'
-
-  get 'cocacola' => 'top#cocacola'
-  get 'asahi' => 'top#asahi'
-  get 'otsuka' => 'top#otsuka'
-  get 'itoen' => 'top#itoen'
-  get 'dydo' => 'top#dydo'
-  get 'kirin' => 'top#kirin'
 
   resources :columns
 
@@ -34,24 +22,6 @@ Rails.application.routes.draw do
       post :send_mail
       get :confirm_point
       post :apply
-    end
-  end
-
-  # メッセージ
-  resources :messages, only: [:create] do
-    collection do
-      # メッセージルーム
-      get :room, path: '/room/:uri_token'
-    end
-  end
-
-  #企業側アカウント
-  resources :companies do
-    collection do
-      get :pay
-      post :get_point
-      post :confirm
-      post :thanks
     end
   end
 
